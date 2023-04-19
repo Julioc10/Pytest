81958dbf30431287729c9e647f8362a2defde0a2
Julioc10 <ojulicesar010@gmail.com>
2023-04-19 10:39:33 -0300

    Commit test_numbers

---
+++ test_num.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_num():
+    if __name__ == '__main__':
+    
+        a = [1, 2, 3, 4, 5]
+        a.reverse()
+        print(a)
+
+def resp():
+    assert test_num == [5, 4, 3, 2, 1]
+    
